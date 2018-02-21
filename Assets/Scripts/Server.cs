6ec4b913db2663b6d95f7de975de26e04f2ec185
Ar11rA <aritra.ghosh2295@yahoo.com>
2018-02-21 15:41:15 +0530

    modified server file

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -5,6 +5,37 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
+public class Player {
+	public string name;
+	public string move;
+	public int score;
+
+	public Player() {
+		name = null;
+		move = null;
+		score = 0;
+	}
+
+}
+
+public class RuleMatrix {
+	public Dictionary<string, int> rules;
+
+	public RuleMatrix() {
+		rules = new Dictionary<string, int> ();
+		rules.Add("rockrock", 0);	
+		rules.Add("rockpaper", 2);
+		rules.Add("rockscissor", 1);
+		rules.Add("paperrock", 1);
+		rules.Add("paperpaper", 0);
+		rules.Add("paperscissor", 2);
+		rules.Add("scissorrock", 2);
+		rules.Add("scissorpaper", 1);
+		rules.Add("scissorscissor", 0);
+	}
+
+}
+
 public class Server : MonoBehaviour
 {
 
@@ -15,6 +46,8 @@ public class Server : MonoBehaviour
     private int MAX_CONNECTION = 2;
     private int port = 5701;
     private Text ServerText;
+	private int numberOfMovesReceived = 0;
+	public Dictionary<int, Player> players = new Dictionary<int, Player> ();
 
     void Start()
     {
@@ -41,18 +74,69 @@ public class Server : MonoBehaviour
         NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
         switch (recData)
         {
-            case NetworkEventType.Nothing:         //1
+            case NetworkEventType.Nothing:         
                 break;
-            case NetworkEventType.ConnectEvent:    //2
+            case NetworkEventType.ConnectEvent:    
 				Debug.Log("Client connected with connection id - " + connectionId);
                 ServerText.text = "Client connected with connection id - " + connectionId;
                 break;
-            case NetworkEventType.DataEvent:       //3
-				ServerText.text = "data received from " + connectionId + Encoding.Unicode.GetString(recBuffer, 0, dataSize);;
+			case NetworkEventType.DataEvent:       
+				string message = Encoding.Unicode.GetString (recBuffer, 0, dataSize);
+				ServerText.text = "Data received from " + connectionId + " " + message;
+				string[] messageInfo = message.Split ('|');
+				string command = messageInfo [0];
+				switch (command) {
+					case "MOVE": 
+						onMove (connectionId, messageInfo[1]);
+						break;
+					case "NAME":
+						onConnection (connectionId, messageInfo[1]);
+						break;
+				}
                 break;
-            case NetworkEventType.DisconnectEvent: //4
+            case NetworkEventType.DisconnectEvent: 
                 ServerText.text = "Client " + connectionId + "disconnected";
                 break;
         }
     }
+
+	void onConnection(int connectionId, string name) {
+		Debug.Log ("On new player: " + name);
+		Player newPlayer = new Player ();
+		newPlayer.name = name;
+		players.Add(connectionId, newPlayer); 
+	}
+
+	void onMove(int connectionId, string move) {
+		Debug.Log ("On new move: " + move);
+		Debug.Log ("Player connection ID:" + connectionId);
+		foreach(var player in players)
+		{
+			Debug.Log (player.Value.name);
+		}
+		players [connectionId].move = move;
+		numberOfMovesReceived = 0;
+		foreach(var player in players)
+		{
+			if (player.Value.move != null)
+				numberOfMovesReceived++;
+		}
+		Debug.Log (numberOfMovesReceived);
+		if (numberOfMovesReceived == 2) {
+			getResult ();
+		}
+		Debug.Log (players.ToString ());
+	}
+
+	void getResult() {
+		RuleMatrix ruleMatrix = new RuleMatrix();
+		string finalMove = "";
+		foreach(var player in players)
+		{
+			finalMove += player.Value.move;
+		}
+		Debug.Log (finalMove);
+		int result = ruleMatrix.rules [finalMove];
+		Debug.Log (result);
+	}
 }
